e74a00234cce9a920f5648e97d6a26aa7b0d0f04
ZachGraceffa <zachgraceffa@gmail.com>
2021-01-31 23:31:50 -0600

    Added sample data and data retrieval from blog post: https://dev.to/cmohan/build-a-basic-gatsby-blog-with-markdown-posts-2ne8

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,23 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+      title: `Furniture Portfolio`,
+      description: `Place to display my stuff.`,
+  },
+  plugins: [
+      {
+          resolve: `gatsby-plugin-mdx`,
+          options: {
+              extensions: [`.mdx`, `.md`],
+          },
+      },
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              name: `posts`,
+              path: `${__dirname}/src/furniture`,
+          },
+      },
+  ],
 }
